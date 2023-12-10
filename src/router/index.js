7cbf9b88992e44bac45b264c0d2f77e71d276ad8
nittaya.cho <nittaya.cho@inet.co.th>
2023-12-10 15:06:44 +0700

    fix call api, use dynamic router, create component

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
+import EventDetailsView from '../views/EventDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'vent-list',
+      name: 'event-list',
       component: EventListView
+    },
+    {
+      path: '/event/:id',
+      name: 'event-details',
+      props: true,
+      component: EventDetailsView
     },
     {
       path: '/about',
