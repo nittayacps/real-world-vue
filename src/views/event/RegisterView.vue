b05989af7b026aac6b9584c6890155663e43660a
nittaya.cho <nittaya.cho@inet.co.th>
2023-12-10 16:29:52 +0700

    add programatic navigation

--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { useRouter } from 'vue-router';
+
 const props = defineProps({
     event: { required: true}
 })
+const router = useRouter()
+const register = () => {
+    router.push({
+        name: 'event-details'
+    })
+}
 </script>
 
 <template>
     <p>Register event here</p>
+    <button @click="register">Register</button>
 </template>
